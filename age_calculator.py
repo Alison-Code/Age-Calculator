f79bcde10b6cb829af6da3e41baf52b2ba32f50a
Alison-Code <72189697+Alison-Code@users.noreply.github.com>
2021-10-01 09:59:21 +0530

    Create age_calculator.py

---
+++ age_calculator.py
@@ -0,0 +1,15 @@
+yearage = int(input("What is your age/Year of birth: "))
+year = 0
+age = 0
+
+if yearage < 100 and yearage > 0:
+    year = 2021 - yearage
+    print(f"You will turn 100 in the year {year+100}(after {100-age} years)")
+
+elif yearage > 1900 and yearage < 2021:
+    year = yearage
+    age = 2021 - yearage
+    print(f"You will turn 100 in the year {year+100}(after {100-age} years)")
+
+elif yearage > 100 and yearage < 150:
+    print(f"You have already turn 100")
